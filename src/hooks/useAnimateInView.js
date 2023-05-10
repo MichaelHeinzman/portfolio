02675b73bcd27f7b05cabd3904fee46981fb6f3b
MichaelHeinzman <seth@heinzman.net>
2023-05-10 10:57:56 -0400

    Updated animation to handle screen sizes.

--- src/hooks/useAnimateInView.js
+++ src/hooks/useAnimateInView.js
@@ -4,7 +4,24 @@ import { useInView } from "react-intersection-observer";
 
 const useAnimateInView = () => {
   const controls = useAnimation();
-  const [ref, inView] = useInView();
+  const [ref, inView] = useInView({
+    threshold: 0.45, // Set intersection threshold for triggering animation
+  });
+
+  const getBreakpointX = () => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 600) {
+      // xs screen size
+      return "50px";
+    } else if (screenWidth >= 600 && screenWidth < 960) {
+      // md screen size
+      return "75px";
+    } else {
+      // lg screen size and above
+      return "200px";
+    }
+  };
 
   const variants = {
     text: {
@@ -13,7 +30,7 @@ const useAnimateInView = () => {
     },
     svg: {
       visible: { opacity: 1, scale: 1, transition: { duration: 1 }, x: 0 },
-      hidden: { opacity: 0, scale: 0, x: 250 },
+      hidden: { opacity: 0, scale: 0, x: getBreakpointX() },
     },
   };
 
